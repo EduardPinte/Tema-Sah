3afd3b47acb5d13f995bd8fbf9eb07a58c4d5c3b
PinteEduard <eduard.pinte@emanuel.ro>
2025-05-15 10:53:32 +0300

    Am creat fetele zambitoare

--- script.js
+++ script.js
@@ -1,26 +1,73 @@
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(400, 400);
+  noLoop(); 
 }
 
 function draw() {
-     background("grey");
-     let coloane = 8;
-     let randuri =8;
-     let cellSize= width / coloane;
+  background("grey");
+  let coloane = 8;
+  let randuri = 8;
+  let cellSize = width / coloane;
 
-     for(let i=0; i<coloane; i++){
-        for(let j=0; j<randuri; j++){
-            if((i+j)%2 == 0){
-                fill("white");
-            }else{
-                fill("black");
-            }
-            rect(i * cellSize, j*cellSize  , cellSize,cellSize);
-        }
-     }
+  for (let i = 1; i <= coloane; i++) {
+    for (let j = 1; j <= randuri; j++) {
+      if ((i + j) % 2 == 0) {
+        fill("white");
+      } else {
+        fill("black");
+      }
 
+      rect((i - 1) * cellSize, (j - 1) * cellSize, cellSize, cellSize);
+
+      let x = (i - 1) * cellSize + cellSize / 2;
+      let y = (j - 1) * cellSize + cellSize / 2;
+      let d = cellSize * 0.6;
+
+     
+      if (i % 2 == 0 && j == 2) {
+        smileyWhite(x, y, d, "grey");
+      }
+      if (j == 3 && (i == 1 || i == 5)) {
+        smileyWhite(x, y, d, "grey");
+      }
+      if (j == 4 && (i == 3 || i == 7)) {
+        smileyWhite(x, y, d, "grey");
+      }
+      if (j == 5 && (i == 2 || i == 5)) {
+        smileyWhite(x, y, d, "white");
+      }
+      if (j == 6 && (i == 4 || i == 6 || i == 8)) {
+        smileyWhite(x, y, d, "white");
+      }
+      if (j == 7 && (i == 1 || i == 3 || i == 7)) {
+        smileyWhite(x, y, d, "white");
+      }
+    }
+  }
 }
 
-function smileyWhite(){
+// 🔵 Smiley face în cerc
+function smileyWhite(x, y, diameter, faceColor) {
+  // Fața
+  fill(faceColor);
+  stroke(0);
+  strokeWeight(1);
+  circle(x, y, diameter);
 
+  // Ochi
+  let eyeOffsetX = diameter * 0.2;
+  let eyeOffsetY = diameter * 0.2;
+  let eyeSize = diameter * 0.1;
+
+  fill(0); // negru
+  noStroke();
+  circle(x - eyeOffsetX, y - eyeOffsetY, eyeSize); // ochi stâng
+  circle(x + eyeOffsetX, y - eyeOffsetY, eyeSize); // ochi drept
+
+  // Gură
+  noFill();
+  stroke(0);
+  strokeWeight(2);
+  let mouthRadius = diameter * 0.25;
+  arc(x, y + diameter * 0.1, mouthRadius * 2, mouthRadius, 0, PI); // arc zâmbet
 }
